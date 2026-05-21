2ea68b42753d725bb16b6948c1d6f962f4726302
Abraham_Jxhn <111785875+Abraham-John@users.noreply.github.com>
2026-05-21 21:06:48 +0530

    Delete ui/ab3.py
    Removed unused files

--- ui/ab3.py
+++
@@ -1,5 +0,0 @@
-employee_name=input("enter employee name\n ")
-employee_desk_number=input("enter employee desk number\n")
-employee_occupation=input("enter employee occupation\n")
-
-print("employee_name,employee_desk_number,employee_occupation")
